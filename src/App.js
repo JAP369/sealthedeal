424626f7e305bc0d509f7fba5405416259c937b9
JonathanPaita <jonathan.austria.paita@gmail.com>
2020-09-09 21:18:26 +0800

    added directory, menu-item components and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import MenuItem from "./components/menu-item/menu-item.component";
+import Directory from "./components/directory/directory.component";
 import "./App.css";
 
 function App() {
